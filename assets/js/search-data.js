6c7d1c6df8625c845c5a5ce25065f922b10bc2c3
uifj <116880469+uifj@users.noreply.github.com>
2025-04-01 15:35:13 +0000

    Deploying to gh-pages from @ uifj/uifj.github.io@dfbc027e156c90bbd96c7736aa5f618ef1796f2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-简历",
+          title: "简历",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
