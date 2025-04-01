7f2f2bab1044979e1f7a9a4e8edfc6aec3556975
uifj <116880469+uifj@users.noreply.github.com>
2025-04-01 15:58:58 +0000

    Deploying to gh-pages from @ uifj/uifj.github.io@64dea1682b6e55ca76ed25770f6373407e395c85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-代码仓库",
+          title: "代码仓库",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
