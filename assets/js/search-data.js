873d4bd485955494ff89fe4b12157c865d04765b
uifj <116880469+uifj@users.noreply.github.com>
2025-04-01 15:34:45 +0000

    Deploying to gh-pages from @ uifj/uifj.github.io@faa773a0a54574a389eb70eae904df2d7f20aff8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,8 +44,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-up主",
+          title: "UP主",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
